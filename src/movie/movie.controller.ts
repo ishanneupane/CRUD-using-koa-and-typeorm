1924d6f59988f07f18c1edd6536723828d6a318e
Ishan Neupane <ishanneupane8945@gmail.com>
2024-06-25 17:08:54 +0545

    prettier

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -3,56 +3,62 @@ import { Context } from "koa";
 import * as Router from "koa-router";
 import movieEntity from "../entity/movie";
 import * as HttpStatus from "http-status-codes";
-import {  Repository } from "typeorm";
+import { Repository } from "typeorm";
 
 const routerOpts: Router.IRouterOptions = { prefix: "/movies" };
 const router = new Router(routerOpts);
 
 router.get("/", async (ctx: Context) => {
-  const movieRepo: Repository<movieEntity> =ctx.state.db. getRepository(movieEntity);
+  const movieRepo: Repository<movieEntity> =
+    ctx.state.db.getRepository(movieEntity);
   const movies = await movieRepo.find();
-  ctx.body = { data: movies};
+  ctx.body = { data: movies };
 });
 
 router.get("/:id", async (ctx: Context) => {
-  const movieRepo: Repository<movieEntity> =ctx.state.db.getRepository(movieEntity);
-  const movie = await movieRepo.findOne({where:{id:(ctx as any).params.id as string} } );  // Using type assertion here
+  const movieRepo: Repository<movieEntity> =
+    ctx.state.db.getRepository(movieEntity);
+  const movie = await movieRepo.findOne({
+    where: { id: (ctx as any).params.id as string },
+  }); // Using type assertion here
   if (!movie) {
     ctx.throw(HttpStatus.StatusCodes.NOT_FOUND);
   }
   ctx.body = { data: { movie: movie } };
 });
 
-router.post("/", async (ctx:Context) => {
-  try{
-    
+router.post("/", async (ctx: Context) => {
+  try {
     // const movieRepo: Repository<movieEntity> = getRepository(movieEntity);
-    
-    const movieRepo: Repository<movieEntity> = ctx.state.db.getRepository(movieEntity);
+    const movieRepo: Repository<movieEntity> =
+      ctx.state.db.getRepository(movieEntity);
     const movie = movieRepo.create((ctx as any).request.body);
-    await movieRepo.save(movie); 
- 
+    await movieRepo.save(movie);
     ctx.body = { data: movie };
-  }catch(e){ console.log(e)}
-
-  });
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 router.put("/:id", async (ctx: Koa.Context) => {
-  const movieRepo: Repository<movieEntity> = ctx.state.db. getRepository(movieEntity);
-  const movie = await movieRepo.findOne({where: {id:(ctx as any).params.id as string}});
+  const movieRepo: Repository<movieEntity> =
+    ctx.state.db.getRepository(movieEntity);
+  const movie = await movieRepo.findOne({
+    where: { id: (ctx as any).params.id as string },
+  });
   if (!movie) {
     ctx.throw(HttpStatus.StatusCodes.NOT_FOUND);
   }
   movieRepo.merge(movie, (ctx as any).request.body);
   const updatedMovie = await movieRepo.save(movie);
   ctx.body = { data: updatedMovie };
-
 });
 
 router.delete("/:id", async (ctx: Koa.Context) => {
-  const movieRepo: Repository<movieEntity> = ctx.state.db. getRepository(movieEntity);
-  const movieId = ((ctx as any).params.id)as string;
-  const movie = await movieRepo.findOne({where: {id: movieId}});
+  const movieRepo: Repository<movieEntity> =
+    ctx.state.db.getRepository(movieEntity);
+  const movieId = (ctx as any).params.id as string;
+  const movie = await movieRepo.findOne({ where: { id: movieId } });
   if (!movie) {
     ctx.throw(HttpStatus.StatusCodes.NOT_FOUND);
   }
